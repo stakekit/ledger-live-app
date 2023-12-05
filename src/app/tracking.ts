ad953c0bf91d29dc1b13396a1d80175668a51d30
Petar Todorovic <petar@omni.app>
2023-12-05 14:19:56 +0100

    feat(): intercom chat

--- src/app/tracking.ts
+++ src/app/tracking.ts
@@ -2,13 +2,10 @@ import { TrackingContextProvider } from "@stakekit/widget"
 import mixpanel from "mixpanel-browser"
 import { ComponentProps } from "react"
 
-export const tracking: ComponentProps<
-  typeof TrackingContextProvider
->["tracking"] = {
-  trackEvent: (...args) => {
-    mixpanel.track(...args)
-  },
-  trackPageView: (page, props) => {
-    mixpanel.track_pageview({ page, ...(props && { props }) })
-  },
+export const tracking: NonNullable<
+  ComponentProps<typeof TrackingContextProvider>["tracking"]
+> = {
+  trackEvent: (...args) => mixpanel.track(...args),
+  trackPageView: (page, props) =>
+    mixpanel.track_pageview({ page, ...(props && { props }) }),
 }
--- src/app/widget/index.tsx
+++ src/app/widget/index.tsx
@@ -5,18 +5,59 @@ import { SKApp, darkTheme } from "@stakekit/widget"
 import { config } from "../../config"
 import mixpanel from "mixpanel-browser"
 import { tracking } from "../tracking"
+import Script from "next/script"
+import { useState } from "react"
 
-if (typeof window !== "undefined" && config.mixPanelToken) {
+typeof window !== "undefined" &&
+  config.mixPanelToken &&
   mixpanel.init(config.mixPanelToken)
-}
 
 export const Widget = () => {
+  const [address, setAddress] = useState<string | undefined>(undefined)
+
   return (
-    <SKApp
-      apiKey={config.apiKey}
-      theme={darkTheme}
-      connectKitForceTheme="darkMode"
-      tracking={tracking}
-    />
+    <>
+      <SKApp
+        apiKey={config.apiKey}
+        theme={darkTheme}
+        connectKitForceTheme="darkMode"
+        tracking={{
+          ...tracking,
+          trackEvent: (...args) => {
+            const event = args[0]
+
+            switch (event) {
+              case "Connected wallet":
+                window.Intercom?.("update", { address: args[1]?.address })
+                setAddress(args[1]?.address)
+                break
+
+              case "Widget disconnect clicked":
+                window.Intercom?.("update", { address: undefined })
+                setAddress(undefined)
+                break
+
+              default:
+                break
+            }
+
+            tracking.trackEvent(...args)
+          },
+        }}
+      />
+
+      <Script
+        src="/chat.js"
+        strategy="afterInteractive"
+        onLoad={() =>
+          typeof window !== "undefined" &&
+          window.Intercom!("boot", {
+            api_base: config.intercom.apiBase,
+            app_id: config.intercom.appId,
+            address,
+          })
+        }
+      />
+    </>
   )
 }
