75e43590214a4dd16fa33e1dbbcaf68f78040898
Petar Todorovic <petar@omni.app>
2024-03-19 10:23:53 +0100

    feat(): intercom test

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export const config = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY ?? "",
   mixPanelToken: process.env.NEXT_PUBLIC_MIX_PANEL_TOKEN ?? "",
-  intercom: { apiBase: "https://api-iam.intercom.io", appId: "b2he0jzb" },
+  intercom: { apiBase: "https://api-iam.intercom.io", appId: "k65tds23" },
 } as const
