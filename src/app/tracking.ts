2faf5cf69b62f10502ab316a7e67e41548d0acc2
Petar Todorovic <petar@omni.app>
2024-05-13 14:40:54 +0200

    feat(): bump widget

--- src/app/tracking.ts
+++ src/app/tracking.ts
@@ -1,10 +1,7 @@
-import { TrackingContextProvider } from "@stakekit/widget"
+import { SKAppProps } from "@stakekit/widget"
 import mixpanel from "mixpanel-browser"
-import { ComponentProps } from "react"
 
-export const tracking: NonNullable<
-  ComponentProps<typeof TrackingContextProvider>["tracking"]
-> = {
+export const tracking: NonNullable<SKAppProps["tracking"]> = {
   trackEvent: (...args) => mixpanel.track(...args),
   trackPageView: (page, props) =>
     mixpanel.track_pageview({ page, ...(props && { props }) }),
