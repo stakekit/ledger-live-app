c5d4406b05864774464e0d63dadd7ea7c4638968
Petar Todorovic <petar@omni.app>
2024-08-14 12:56:13 +0200

    feat(): bump widget, background color change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,10 @@ import { Widget } from "./widget"
 import { HelpModals } from "./widget/help-modals"
 import { Box } from "../components/atoms/box"
 import { container } from "./style.css"
-import { FluidContainer } from "./fluid"
 
 export default function Home() {
   return (
     <Box className={container}>
-      <FluidContainer />
       <Header />
       <Widget />
       <Box marginTop={{ tablet: "0", mobile: "8" }}>
