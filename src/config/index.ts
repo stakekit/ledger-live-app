6e0a6e2d3a599288cbac6bb71c4f9daac43982b0
Petar Todorovic <petar@stakek.it>
2026-05-13 17:43:25 +0200

    feat: testing stg

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
 const env = import.meta.env;
 
 export const config = {
-	apiKey: env.VITE_API_KEY ?? env.NEXT_PUBLIC_API_KEY ?? "",
-	baseUrl: env.VITE_BASE_URL ?? env.NEXT_PUBLIC_BASE_URL ?? undefined,
+	apiKey: "e199a0f3-cb4e-4022-9e03-01430ce0f600",
+	baseUrl: "https://api.stg.stakek.it/",
 	mixPanelToken:
 		env.VITE_MIX_PANEL_TOKEN ?? env.NEXT_PUBLIC_MIX_PANEL_TOKEN ?? "",
 } as const;
